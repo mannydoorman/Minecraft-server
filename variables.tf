28499aa79f73fcbf3c61a3d3d0f00429c812bff6
mannydoorman <147976825+mannydoorman@users.noreply.github.com>
2025-06-11 23:33:54 -0700

    Delete variables.tf

--- variables.tf
+++
@@ -1,4 +0,0 @@
-variable "aws_region"      { default = "us-east-1" }
-variable "ami_id"          { default = "ami-084568db4383264d4" }  # Ubuntu AMI in your region
-variable "instance_type"   { default = "t2.small" }
-variable "public_key_path" { default = "~/.ssh/id_rsa.pub" }
